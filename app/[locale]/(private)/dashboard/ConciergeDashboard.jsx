115072ada237548a527645a1cc492338fc19a713
Matías Olea <matolea@alumnos.uai.cl>
2024-06-20 00:03:27 -0400

    CAT-102 vista dashboard concerge teminada

--- app/[locale]/(private)/dashboard/ConciergeDashboard.jsx
+++ app/[locale]/(private)/dashboard/ConciergeDashboard.jsx
@@ -1,14 +1,109 @@
-import Dashboard_component from "@/app/components/ResidentDashboardComp";
-import { logger } from "@/logger"
+import ConciergeDashboardComp from "@/app/components/ConciergeDashboardComp";
+import { logger } from "@/logger";
 import { auth } from "@/auth";
-/*
-    * This is the page the user sees upon logging in. This dashboard will be private, meaning it will only be visible once logged in with email and password.
-*/
-//TODO: this could be a single component instead of a function calling another component
-export default async function ConciergeDashboard(){
-    const session =  await auth();
-    logger.info(`User with email '${session?.user?.email}' has opened the dashboard.`);
+import { redirect } from "next/navigation";
+import { db } from "@vercel/postgres";
 
-    //  Here is a welcome message and a logout button displayed.
-    return <Dashboard_component />;
-};
+/*
+ * This is the page the user sees upon logging in. This dashboard will be private, meaning it will only be visible once logged in with email and password.
+ */
+export default async function ConciergeDashboard() {
+	const session = await auth();
+	if (!session) {
+		redirect("/login");
+	}
+
+	const client = await db.connect();
+
+	let pendingPackages;
+	let parkingOccupancy;
+	let overdueParking;
+	let latestVisitors;
+
+	try {
+		pendingPackages = await client.sql`
+        SELECT
+            p.id,
+            sender, 
+            recipient,
+            TO_CHAR(drop_off, 'YYYY-mm-DD') AS drop_off,
+            community_address
+        FROM 
+            package p
+        INNER JOIN 
+            residence r ON r.id = p.residence_id
+        WHERE 
+            pick_up IS NULL 
+            AND r.community_id = ${session.user.community_id};`;
+
+		parkingOccupancy = await client.sql`
+        WITH total_spaces AS (
+            SELECT COUNT(*) as total_spaces
+            FROM parking_space
+            where community_id = ${session.user.community_id})
+        SELECT 
+            count(psu.id)/(SELECT total_spaces FROM total_spaces)*100 AS usage_percentaje
+        FROM 
+            parking_space_usage psu
+        INNER JOIN
+            visit_to_residence vtr ON vtr.id = psu.visit_id
+        INNER JOIN 
+            parking_space ps ON ps.id = psu.parking_space_id
+        WHERE 
+            vtr.departure IS null;`;
+		overdueParking = await client.sql`
+        SELECT
+            count(psu.id) AS overdue
+        FROM
+            parking_space_usage psu
+        INNER JOIN 
+            visit_to_residence vtr ON vtr.id = psu.visit_id
+        INNER JOIN 
+            parking_space ps ON ps.id = psu.parking_space_id
+        WHERE 
+            vtr.departure is null 
+            AND NOW() - vtr.arrival > INTERVAL '1 day'`;
+
+		//TODO: the interval should come from the config not the code
+		latestVisitors = await client.sql`
+        SELECT
+            vtr.id,
+            CONCAT(v.firstname,' ', v.lastname) AS name,
+            TO_CHAR(vtr.arrival,'YYYY-MM-DD HH24:MM:SS') arrival_time,
+            r.community_address
+        FROM 
+            visit_to_residence vtr
+        INNER JOIN
+            visitor v on v.id = vtr.visitor_id
+        INNER JOIN
+            residence r on r.id = vtr.residence_id
+        WHERE 
+            vtr.departure IS NULL 
+            AND vtr.arrival > NOW() - INTERVAL '1 day'
+        ORDER BY 
+            arrival_time DESC
+        LIMIT 10;`;
+
+		pendingPackages = pendingPackages.rows;
+		parkingOccupancy = parkingOccupancy.rows[0].usage_percentaje;
+		overdueParking = overdueParking.rows[0].overdue;
+		latestVisitors = latestVisitors.rows;
+	} catch (err) {
+		logger.error("Failed to get data for the dashboard", err);
+	} finally {
+		client.release();
+	}
+
+	logger.info(
+		`User with email '${session?.user?.email}' has opened the dashboard.`,
+	);
+	
+	return (
+		<ConciergeDashboardComp
+			pendingPackages={pendingPackages}
+			parkingOccupancy={parkingOccupancy}
+			overdueParking={overdueParking}
+			latestVisitors={latestVisitors}
+		/>
+	);
+}
---
+++ app/components/ConciergeDashboardComp.jsx
@@ -0,0 +1,158 @@
+"use client";
+
+import React from "react";
+import {
+	Card,
+	CardHeader,
+	CardContent,
+	Avatar,
+	Grid,
+	Typography,
+	Container,
+} from "@mui/material";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import LocalParkingIcon from "@mui/icons-material/LocalParking";
+import { useTranslation } from "react-i18next";
+
+export default function ConciergeDashboardComp({
+	pendingPackages,
+	parkingOccupancy,
+	overdueParking,
+	latestVisitors,
+}) {
+	const { t } = useTranslation("translate-dashboard");
+	return (
+		<Container component="main" style={{ flex: 1, padding: "16px" }}>
+			<div style={{ textAlign: "center", marginBottom: "32px" }}>
+				<Typography variant="h4" component="h1" color="primary" gutterBottom>
+					{t("welcome")}
+				</Typography>
+				<Typography variant="body1" color="secondary">
+					{t("subtitle")}
+				</Typography>
+			</div>
+			<Grid container spacing={4}>
+				<Grid item xs={12} sm={6} lg={4}>
+					<Card>
+						<CardHeader title={t("pending_packages")} />
+						<CardContent
+							sx={{
+								minHeight: "22vh",
+								maxHeight: "50vh",
+								overflowY: "scroll",
+							}}
+						>
+							{pendingPackages.map((packageitem) => (
+								<PackageItem key={packageitem.id} data={packageitem} />
+							))}
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<Card>
+						<CardHeader title={t("parking_ocupancy")} />
+						<CardContent sx={{ minHeight: "22vh" }}>
+							<div
+								style={{
+									display: "flex",
+									alignItems: "center",
+									marginBottom: "8px",
+								}}
+							>
+								<LocalParkingIcon style={{ marginRight: "8px" }} />
+								<div>
+									<Typography variant="body1">
+										{t("visitor_parking")}
+									</Typography>
+									<Typography variant="body2" color="textSecondary">
+										{t("percentage", { percentage: parkingOccupancy })}
+									</Typography>
+								</div>
+							</div>
+							<div
+								style={{
+									display: "flex",
+									alignItems: "center",
+									marginBottom: "8px",
+								}}
+							>
+								<LocalParkingIcon style={{ marginRight: "8px" }} />
+								<div>
+									<Typography variant="body1">
+										{t("overdue_parking")}
+									</Typography>
+									<Typography variant="body2" color="textSecondary">
+										{t("overdue", { overdue: overdueParking })}
+									</Typography>
+								</div>
+							</div>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<Card>
+						<CardHeader title={t("visitors")} />
+						<CardContent
+							sx={{
+								minHeight: "22vh",
+								maxHeight: "50vh",
+								overflowY: "scroll",
+							}}
+						>
+							{latestVisitors.map((visitor) => (
+								<VisitorItem key={visitor.id} data={visitor} />
+							))}
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+		</Container>
+	);
+}
+
+function PackageItem({ data }) {
+	const { t } = useTranslation("translate-dashboard");
+	return (
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				marginBottom: "8px",
+			}}
+		>
+			<LocalShippingIcon style={{ marginRight: "8px" }} />
+			<div>
+				<Typography variant="body1">
+					{t("package_for")} {data.recipient}
+				</Typography>
+				<Typography variant="body2" color="textSecondary">
+					{t("sent_by")} {data.sender}
+				</Typography>
+				<Typography variant="body2" color="textSecondary">
+					{t("arrived_on")} {data.drop_off}
+				</Typography>
+			</div>
+		</div>
+	);
+}
+
+function VisitorItem({ data }) {
+	const { t } = useTranslation("translate-dashboard");
+	return (
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				marginBottom: "8px",
+			}}
+		>
+			<Avatar style={{ marginRight: "8px" }} alt={data.name} />
+			<div>
+				<Typography variant="body1">{data.name}</Typography>
+				<Typography variant="body2" color="textSecondary">
+					{t("visiting")} {data.community_address}
+				</Typography>
+			</div>
+		</div>
+	);
+}
