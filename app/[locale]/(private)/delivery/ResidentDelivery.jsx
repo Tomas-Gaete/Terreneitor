49bb244e79ccc82932e26ca32a8dc8cc6ebbf789
Tomas-Gaete <tgaeteb@gmail.com>
2024-06-23 18:09:11 -0400

    Se añadio query que permite mostrar los paquetes históricos de la residencia que ya fueron retirados

--- app/[locale]/(private)/delivery/ResidentDelivery.jsx
+++ app/[locale]/(private)/delivery/ResidentDelivery.jsx
@@ -10,6 +10,8 @@ export default  async function ResidentDelivery() {
   if (!session?.user || !session?.user?.email) return null;
 
   let pendingPackages;
+  let Historic_query;
+  let AllTimePackages;
   let query;
   let name_query;
   let fullname;
@@ -38,14 +40,9 @@ export default  async function ResidentDelivery() {
               recipient: `${row.recipient}`,
               drop_off: `${row.drop_off}`,
               community_address: `${row.community_address}`
+      
             }));
-
-          }
-         catch (error) {
-          console.error("Error loading packages:", error);
-          pendingPackages= [];
-        }
-        name_query =await sql`
+            name_query =await sql`
         SELECT
             firstname,
             lastname
@@ -57,6 +54,37 @@ export default  async function ResidentDelivery() {
           firstname: `${row.firstname}`,
           lastname: `${row.lastname}`
         }));
+            Historic_query = await sql`
+        SELECT
+            p.id,
+            sender, 
+            recipient,
+            TO_CHAR(drop_off, 'YYYY-mm-DD') AS drop_off,
+            community_address
+        FROM 
+            package p
+        INNER JOIN 
+            residence r ON r.id = p.residence_id
+        INNER JOIN
+            resident res ON res.residence_id = r.id
+        WHERE res.id = ${session.user.id};
+            `;
+            AllTimePackages = Historic_query.rows.map(row => ({
+              id: row.id,
+              sender: `${row.sender}`,
+              recipient: `${row.recipient}`,
+              drop_off: `${row.drop_off}`,
+              community_address: `${row.community_address}`
+      
+            }));
+
+          }catch (error) {
+          console.error("Error loading packages:", error);
+          pendingPackages= [];
+          AllTimePackages= [];
+          fullname= [];
+        }
+        
 
     const userFullname = fullname.length > 0 ? fullname[0].result : '';
   return (
